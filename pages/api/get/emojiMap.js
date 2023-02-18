440e590322fc9a4f898a22381e767dfd48367a15
Dillon Barnes <83948303+DillonB07@users.noreply.github.com>
2023-02-18 18:59:42 +0000

    Remove unused files

--- pages/api/get/emojiMap.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import {emojis} from '../../../utils/constants'
-
-export default function handler(req, res) {
-  let emoji = emojis[Math.floor(Math.random() * emojis.length)]
-
-  res.status(200).json({ emoji })
-}
